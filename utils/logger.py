ba0046a4580c8412a37c85737c73832e0b7564a4
Victor Hugo Oliveira da Silva <victorsilva698@gmail.com>
2022-03-06 20:00:31 -0300

    create logger for help app monitoring

---
+++ utils/logger.py
@@ -0,0 +1,26 @@
+from logging import StreamHandler, Formatter
+from logging import Logger
+from constants import LOG_LEVEL
+from collections import defaultdict
+from sys import stdout
+
+
+class BaseLogger(Logger):
+    levels = defaultdict(
+        lambda: 20,
+        {"DEBUG": 10, "INFO": 20, "WARN": 30, "ERROR": 40, "CRITICAL": 50},
+    )
+
+    def __init__(self):
+        stream_handler = StreamHandler(stdout)
+        fmt = Formatter("[%(levelname)s] (%(asctime)s) : %(message)s")
+        stream_handler.setFormatter(fmt)
+        self.addHandler(stream_handler)
+        self.setLevel(self.levels[LOG_LEVEL])
+
+    def post_info(self, operation_message: str):
+        message = f"(POST) - {operation_message}"
+        self.info(msg=message)
+
+
+base_logger = BaseLogger()
