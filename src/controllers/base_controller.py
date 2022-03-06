080380ee0f22489a62b20304670804e1d34344e8
Victor Hugo Oliveira da Silva <victorsilva698@gmail.com>
2022-03-06 20:03:16 -0300

    Create base controller
    create a controller that will be inherited by
    another controller, this base_controller
    aims to deal with open/close repetition code

---
+++ src/controllers/base_controller.py
@@ -0,0 +1,27 @@
+# from connectors.db import mysql_connector, Base
+# from pydantic import BaseModel
+from connectors.sqlite_connector import db_connector
+from utils.logger import base_logger
+
+
+class BaseController:
+    def __init__(self) -> None:
+        self.database_connector = db_connector
+        self.Session = self.database_connector.Session
+
+    def __enter__(self):
+        base_logger.info("Opening db session...")
+        self.session = self.Session()
+        return self.session
+
+    def __exit__(self, *exc):
+        base_logger.info("Closing db session...")
+        self.session.close()
+        base_logger.info("Closed db session")
+
+    def to_dict(self, row) -> dict:
+        data_dict = {}
+        for column in row.__table__.columns:
+            data_dict[column.name] = str(getattr(row, column.name))
+
+        return data_dict
